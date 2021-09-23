54471cee0acb45b9cd520bdb743d5a5478bb6550
Eduardo paiva <eduardok86@gmail.com>
2021-09-23 16:11:52 -0300

    Tela NewRoom e autenticação com o google

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,10 +2,25 @@ import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
+import { useHistory } from 'react-router';
+
+import { auth, firebase } from '../service/firebase'
+
 import '../styles/auth.scss'
 import { Button } from '../components/Button'
 
 export function Home() {
+    const history = useHistory();
+
+    function handleCreateRoom() {
+        const provider = new firebase.auth.GoogleAuthProvider();
+
+        auth.signInWithPopup(provider).then(result =>{
+            console.log(result);
+            history.push('/Room/new');
+        }) 
+    }
+
     return(
         <div id="page-auth">
             <aside >
@@ -16,7 +31,7 @@ export function Home() {
             <main>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
-                    <button className="create-room">
+                    <button onClick={handleCreateRoom} className="create-room">
                     <img src={googleIconImg} alt="Letmeask" />
                     Crie sua sala com o Google
                     </button>
@@ -29,7 +44,6 @@ export function Home() {
                         <Button type="submit">
                             Entrar na sala
                         </Button>
-                        
                     </form>
                 </div>
             </main>
